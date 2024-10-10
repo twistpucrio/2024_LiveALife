b462f9defd693e320b109155c947fca001079ac1
HannaEpelboim <hannaepelboim@tecgraf.puc-rio.br>
2024-10-10 16:06:39 -0300

    Adicionar campo favorito na pg shop, botao fav no shop

--- js/controller/ShopController.js
+++ js/controller/ShopController.js
@@ -2,13 +2,16 @@ const ShopController = ((model, view) => {
     const init = () => {
         model.carregarProdutos().then(() => {
             const produtosPorCategoria = model.getProdutosPorCategoria();
-            view.renderizarCategorias(produtosPorCategoria,adicionarAoCarrinho);
+            view.renderizarCategorias(produtosPorCategoria,adicionarAoCarrinho, adicionarAoFavorito);
 
         });
     };
 
     const adicionarAoCarrinho = (produto) => {
         CarrinhoModel.adicionarItem(produto);     
+    };
+    const adicionarAoFavorito = (produto) => {
+        FavoritoModel.adicionarItem(produto);     
     };
 
     return {
